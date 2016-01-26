73c5b1da62b04c5e8e0a51effd7f022ca69e022f
gregoplex <optiplexx@gmail.com>
2016-01-26 14:02:23 -0500

    replace error with fail

--- pykeg/backup/backup.py
+++ pykeg/backup/backup.py
@@ -53,7 +53,7 @@ import zipfile
 from pykeg.core.util import get_version
 from kegbot.util import kbjson
 
-from .exceptions import BackupError, InvalidBackup
+from .exceptions import BackupError, InvalidBackup, AlreadyInstalledError
 
 from django.conf import settings
 from django.core.files.storage import default_storage
@@ -265,6 +265,9 @@ def restore_media(backup_dir, storage):
 
 
 def restore_from_directory(backup_dir, storage=default_storage):
+    if db_impl.is_installed():
+        raise AlreadyInstalledError('You must erase this system before restoring.')
+    
     logger.info('Restoring from {} ...'.format(backup_dir))
 
     metadata = verify_backup_directory(backup_dir)
