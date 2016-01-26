0223b65884507421e58912e64e8bee7dc49cca31
gregoplex <optiplexx@gmail.com>
2016-01-26 10:36:39 -0500

    try to break unit test w/o triggering a lint error

--- pykeg/backup/backup.py
+++ pykeg/backup/backup.py
@@ -265,6 +265,9 @@ def restore_media(backup_dir, storage):
 
 
 def restore_from_directory(backup_dir, storage=default_storage):
+if not db_impl.is_installed():
+        raise AlreadyInstalledError('You must erase this system before restoring.')
+
 
     logger.info('Restoring from {} ...'.format(backup_dir))
 
