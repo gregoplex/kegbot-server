82d73e365c391781f86130d3b18f5ac7928690e7
gregoplex <optiplexx@gmail.com>
2016-01-26 10:49:47 -0500

    learning python

--- pykeg/backup/backup.py
+++ pykeg/backup/backup.py
@@ -265,11 +265,10 @@ def restore_media(backup_dir, storage):
 
 
 def restore_from_directory(backup_dir, storage=default_storage):
+    logger.info('Restoring from {} ...'.format(backup_dir))
     if not db_impl.is_installed():
         raise AlreadyInstalledError('You must erase this system before restoring.')
 
-
-    logger.info('Restoring from {} ...'.format(backup_dir))
 
     metadata = verify_backup_directory(backup_dir)
     current_engine = db_impl.engine_name()
